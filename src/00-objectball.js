708774a04ee92ba081fb9c7ee4b930434a032a9f
Ryan Woodruff <ryanwoodruff2002@gmail.com>
2022-06-27 17:27:47 -0400

    addedd more players

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -13,10 +13,27 @@ function gameObject() {
                     steals: 3,
                     blocks: 1,
                     slamDunks: 1,
-
-
                 }
-                
+                'Reggie Evans': {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    reboounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 7,
+                }
+                'Brooke Lopez': {
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    reboounds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 15,
+                }
             }
         }
 
