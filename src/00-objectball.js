9af3fb741a2e2525b35e31f0b2ec3e14fcdef784
Gabriel Boudreaux <boudreauxgabriel1@gmail.com>
2022-06-27 16:16:18 -0500

    potato

---
+++ src/00-objectball.js
@@ -0,0 +1,28 @@
+function gameObject() {
+    return{
+        home: {
+            teamName:'Brooklyn Nets'
+            colors: 'Black', 'White'
+            players : {
+                'Alan Anderson': {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    reboounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 1,
+
+
+                }
+                
+            }
+        }
+
+
+
+
+    }
+    
+}
